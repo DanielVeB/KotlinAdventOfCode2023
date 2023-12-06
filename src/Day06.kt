113b50a24e02d846728ec15a012276626244b42b
Daniel Kurosz <danielkurosz@gmail.com>
2023-12-06 18:22:47 +0100

    day 6 completed

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,18 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        val boatRaces = BoatRace.fromString(input)
-        return boatRaces.map { it.getWins().size }.reduce { acc, i -> acc * i }
+        val times = input[0].split(" ").filter { it.isNotEmpty() }.drop(1).map { it.toLong() }
+        val distances = input[1].split(" ").filter { it.isNotEmpty() }.drop(1).map { it.toLong() }
+
+        return times.zip(distances).map { BoatRace(it.first, it.second) }
+            .map { it.getWins().size }.reduce { acc, i -> acc * i }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val time = input[0].split(" ").filter { it.isNotEmpty() }.drop(1).reduce { acc, s -> acc + s  }.toLong()
+        val distance = input[1].split(" ").filter { it.isNotEmpty() }.drop(1).reduce { acc, s -> acc + s  }.toLong()
 
+        val firstWinIndex = BoatRace(time,distance).getFirstWinFromLeft()
+
+        return (time - 2 * firstWinIndex + 1).toInt()
     }
 
     val testInput = readInput("Day06_test")
 
     check(part1(testInput) == 288)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 71503)
 
     val input = readInput("Day06")
 
@@ -20,19 +27,18 @@ fun main() {
     part2(input).println()
 }
 
-class BoatRace(private val time: Int, private val record: Int){
-    companion object{
-        fun fromString(input: List<String>): List<BoatRace>{
-            val times = input[0].split(" ").filter { it.isNotEmpty() }.drop(1).map { it.toInt() }
-            val distances = input[1].split(" ").filter { it.isNotEmpty() }.drop(1).map { it.toInt() }
+class BoatRace(private val time: Long, private val record: Long){
 
-            return times.zip(distances).map { BoatRace(it.first, it.second) }
-        }
-    }
-
-    fun getWins(): List<Int>{
+    fun getWins(): List<Long>{
        return 1.rangeTo(this.time).filter {
             (this.time - it) * it > this.record
         }
     }
+
+    fun getFirstWinFromLeft() : Long{
+        return 1.rangeTo(this.time).first {
+            (this.time - it) * it > this.record
+        }
+    }
+
 }
